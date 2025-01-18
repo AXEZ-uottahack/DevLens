f29366d4bc4526a2b7c820dbef0956857dc1d46c
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-01-18 16:35:12 -0500

    Fix navbar alignment

--- index_files/src/app/components/Navbar.tsx
+++ index_files/src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 import LanguageSelector from "./LanguageSelector";
 import { ColorModeButton, useColorMode } from "@/components/ui/color-mode";
 import { Switch } from "@/components/ui/switch";
-import { Box, Text} from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react";
 
 interface NavbarProps {
   language: string; // The current programming language
@@ -11,22 +11,20 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ language, onSelect }) => {
-
-    const { colorMode, toggleColorMode } = useColorMode(); // Hook for light/dark mode
-    const isDark = colorMode === "dark";
+  const { colorMode, toggleColorMode } = useColorMode(); // Hook for light/dark mode
+  const isDark = colorMode === "dark";
   return (
     <div className="flex flex-row items-center justify-between p-3 text-white w-full">
       <Logo size="text-2xl" theme="Dark" />
-      <div id="editor-nav"
-    className="absolute right-1/2  flex items-center">
+      <div
+        id="editor-nav"
+        className="absolute right-1/2  flex content-center text-center"
+      >
         <LanguageSelector language={language} onSelect={onSelect} />
       </div>
       <div className="flex flex-row gap-2">
-        <Text>
-            {isDark ? "Dark Mode" : "Light Mode"}
-        </Text>
-        <ColorModeButton/>
-        
+        <Text className="pt-1">{isDark ? "Dark Mode" : "Light Mode"}</Text>
+        <ColorModeButton />
       </div>
     </div>
   );
