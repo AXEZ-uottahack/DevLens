bdfea4603e3151fc7a4ffce829b98de0f08e4d66
Zeke <ishob080@uottawa.ca>
2025-01-18 15:33:18 -0500

    Modified the JSON structure of the UML prompt

--- gemini_test/gemini-fast.js
+++ gemini_test/gemini-fast.js
@@ -12,7 +12,7 @@ ${code}
 Generate a JSON representation of the UML class diagram:
 Use the following structure:
 JSON
-{ "classes": [ { "name": <name>, "attributes": [{ "type": <type>, "modifier": <modifier> }] }, ... ], "associations": [ { "start": <class name>, "end": <class name>, "start_m": <multiplicity at start>, "end_m": <multiplicity at end>, "bidir": <boolean (true if bidirectional, false otherwise)> }, ... ] }
+{ "classes": [ { "name": "<name>", "attributes": [{ "type": "<type>", "modifier": "<modifier>", "name": "<attribute name>" }] }, ... ], "associations": [ { "start": "<class name>", "end": "<class name>", "start_m": "<multiplicity at start>", "end_m": "<multiplicity at end>", "bidir": <boolean (true if bidirectional, false otherwise)> }, ... ] }
 Include class names, attributes (with types and modifiers), and associations with multiplicities.
 If an association is bidirectional, represent it only once in the "associations" array.
 Ensure the JSON representation is accurate and adheres to the specified structure.`;}
