5b772125028680df3e7fb3630f5e774e25914ac8
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-01-18 22:48:25 -0500

    Changed navbar using flex/absolute to grid to align buttons and logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,39 +17,55 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ language, onSelect }) => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className="flex flex-row items-center justify-between p-3 text-white w-full">
-      <Logo size="text-2xl" theme={theme} />
-      <div
-        id="editor-nav"
-        className="absolute right-1/2  flex content-center text-center"
-      >
-        <LanguageSelector language={language} onSelect={onSelect} />
+    <div className="grid grid-cols-2 items-center gap-x-5">
+      <div className="grid grid-cols-2 items-center">
+        <Logo size="text-2xl" theme={theme} />
+        <div
+          id="editor-nav"
+          className="flex justify-end content-center text-center"
+        >
+          <LanguageSelector language={language} onSelect={onSelect} />
+        </div>
       </div>
-      <div className="flex flex-row gap-2">
-        <button
-          className={`border px-6 py-2 rounded-md transition-all ${
-            theme === "dark"
-              ? "border-white text-white hover:bg-white hover:text-black"
-              : "border-black text-black hover:bg-black hover:text-white"
-          }`}
-        >
-          Analyze
-        </button>
-        <IconButton
-          onClick={toggleTheme}
-          variant="ghost"
-          aria-label="Toggle color mode"
-          size="sm"
-          css={{
-            _icon: {
-              width: "5",
-              height: "5",
-              color: `${theme === "dark" ? "white" : "black"}`,
-            },
-          }}
-        >
-          {theme === "dark" ? <LuMoon /> : <LuSun />}
-        </IconButton>
+
+      <div className="grid grid-cols-2 items-center">
+        <div>
+          <button
+            className={`border px-6 py-2 rounded-md transition-all ${
+              theme === "dark"
+                ? "border-white text-white hover:bg-white hover:text-black"
+                : "border-black text-black hover:bg-black hover:text-white"
+            }`}
+          >
+            Documentation
+          </button>
+        </div>
+        <div className="flex flex-row gap-2 justify-self-end">
+          <button
+            className={`border px-6 py-2 rounded-md transition-all ${
+              theme === "dark"
+                ? "border-white text-white hover:bg-white hover:text-black"
+                : "border-black text-black hover:bg-black hover:text-white"
+            }`}
+          >
+            Analyze
+          </button>
+          <IconButton
+            onClick={toggleTheme}
+            variant="ghost"
+            aria-label="Toggle color mode"
+            size="sm"
+            css={{
+              _icon: {
+                width: "5",
+                height: "5",
+                color: `${theme === "dark" ? "white" : "black"}`,
+              },
+            }}
+          >
+            {theme === "dark" ? <LuMoon /> : <LuSun />}
+          </IconButton>
+        </div>
       </div>
     </div>
   );
